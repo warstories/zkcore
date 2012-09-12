1136d2af47841fc1ef6b97f6ef1265330375daf3
Polo Ornelas <poloornelas@yahoo.com>
2012-09-11 20:06:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zkcore::Application.config.secret_token = '433671760a776a22f2f8b85181516b1b606d4b5aa390515bbd13403ed84e20e9d6965a4fa1eccdb118e53e547cb7de3dce8d72c5a684a0bf1ffd657789a53d89'
